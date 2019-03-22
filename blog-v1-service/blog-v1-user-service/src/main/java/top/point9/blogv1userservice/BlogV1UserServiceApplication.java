4ef37e941cff8966e5c735bc27c06cf30ed9f334
point9 <blog@point9.top>
2019-03-23 02:57:40 +0800

    添加用户模块开发结构，初次完善项目介绍文档

---
+++ blog-v1-service/blog-v1-user-service/src/main/java/top/point9/blogv1userservice/BlogV1UserServiceApplication.java
@@ -0,0 +1,13 @@
+package top.point9.blogv1userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogV1UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogV1UserServiceApplication.class, args);
+    }
+
+}
---
+++ blog-v1-web/blog-v1-admin-web/src/main/java/top/point9/blogv1adminweb/BlogV1AdminWebApplication.java
@@ -0,0 +1,13 @@
+package top.point9.blogv1adminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogV1AdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogV1AdminWebApplication.class, args);
+    }
+
+}
